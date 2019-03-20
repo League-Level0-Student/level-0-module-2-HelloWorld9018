40c62a370c72dd59098ff2f830be7f3f2aa66ab0
codeclub <codeclub@smms-rm23-24.smusd.local>
2019-03-20 14:59:22 -0700

    stuff

--- src/methods/AnimalFarm.java
+++ src/methods/AnimalFarm.java
@@ -16,7 +16,20 @@ public class AnimalFarm {
 		/*
 		 * 1. Ask the user which animal they want, then play the sound of that animal.
 		 */
+		String animal = JOptionPane.showInputDialog(null, "Choose an animal: cow, duck, or dog.");
+		if (animal.equals("cow")) {
+			playMoo();
+		}
 		
+		if (animal.equals("duck")) {
+			playQuack();
+		}
+		
+		if (animal.equals("dog")) {
+			playWoof();
+		}
+		
+	
 		/* 2. Make it so that the user can keep entering new animals. */
 	}
 
